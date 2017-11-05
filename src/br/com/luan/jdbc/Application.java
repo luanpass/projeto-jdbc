bb0a050f84e2d6cf2a0f9f2b181101449650113c
Renã Passos de Souza <ren.boxe@gmail.com>
2017-11-04 22:22:51 -0200

    entidades

---
+++ src/br/com/luan/jdbc/Application.java
@@ -0,0 +1,9 @@
+package br.com.luan.jdbc;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+	}
+	
+}
---
+++ src/br/com/luan/jdbc/entidades/Artista.java
@@ -0,0 +1,23 @@
+package br.com.luan.jdbc.entidades;
+
+public class Artista {
+	
+	private int codigoArtista;
+	private String nomeArtista;
+	
+	public int getCodigoArtista() {
+		return codigoArtista;
+	}
+	public void setCodigoArtista(int codigoArtista) {
+		this.codigoArtista = codigoArtista;
+	}
+	public String getNomeArtista() {
+		return nomeArtista;
+	}
+	public void setNomeArtista(String nomeArtista) {
+		this.nomeArtista = nomeArtista;
+	}
+	
+	
+
+}
